e2ab9bcc6d82556cee249d29f213aa38d77ed24f
janodin <japatundog@usm.edu.ph>
2024-05-04 13:22:00 +0800

    .

--- app/views.py
+++ app/views.py
@@ -1,10 +1,8 @@
 import base64
-
 import cv2
 import numpy as np
-from django.http import HttpResponse
+from skimage.feature import hog
 from django.shortcuts import render
-
 from app.forms import ImageUploadForm
 import joblib
 
@@ -15,45 +13,64 @@ model = joblib.load('static/svm_model.joblib')
 varieties = ['BR25', 'K-1', 'PBC_123']
 
 
+def extract_hog_features(image):
+    # Convert to grayscale
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # Resize images to ensure uniformity
+    resized_img = cv2.resize(gray_image, (128, 128))
+    # Extract HOG features
+    fd, _ = hog(resized_img, orientations=9, pixels_per_cell=(8, 8),
+                cells_per_block=(2, 2), visualize=True)
+    return fd
+
+
 def index(request):
     if request.method == 'POST':
         form = ImageUploadForm(request.POST, request.FILES)
         if form.is_valid():
-            # Read the uploaded image
-            uploaded_image = form.cleaned_data['image']
-            image = cv2.imdecode(np.frombuffer(uploaded_image.read(), np.uint8), cv2.IMREAD_COLOR)
+            try:
+                # Read the uploaded image
+                uploaded_image = form.cleaned_data['image']
+                image = cv2.imdecode(np.frombuffer(uploaded_image.read(), np.uint8), cv2.IMREAD_COLOR)
 
-            # Preprocess the image (similar to how you did in training)
-            resized = cv2.resize(image, (200, 200))
-            hsv = cv2.cvtColor(resized, cv2.COLOR_BGR2HSV)
-            means, stds = cv2.meanStdDev(hsv)
-            color_features = np.concatenate([means, stds]).flatten()
+                # Preprocess the image and extract features
+                extracted_features = extract_hog_features(image)
 
-            gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
-            blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-            shape_features = cv2.Canny(blurred, 200, 300).flatten()
+                # Predict the probability and class
+                proba = model.predict_proba([extracted_features])[0]
+                prediction = model.predict([extracted_features])[0]
 
-            combined_features = np.concatenate([color_features, shape_features]).flatten()
-            reshaped = combined_features.reshape(1, -1)
+                # Map the numeric prediction back to variety name
+                predicted_label = varieties[prediction]  # Adjust index if necessary
+                max_probability = np.max(proba)
+                accuracy = max_probability * 100  # Convert to percentage
 
-            # Predict the class probabilities using the SVM model
-            probabilities = model.predict_proba(reshaped)[0]
-            predicted_label_index = np.argmax(probabilities)
-            predicted_label = varieties[predicted_label_index]
-            accuracy = probabilities[predicted_label_index] * 100  # Convert to percentage
+                if accuracy < 85:
+                    # Load no_image_found.jpg
+                    with open('static/not_detected.png', 'rb') as image_file:
+                        no_image_data = base64.b64encode(image_file.read()).decode('utf-8')
+                    image_data = f'data:image/jpeg;base64,{no_image_data}'
 
-            # Convert the image to Base64 for displaying on the web
-            _, buffer = cv2.imencode('.jpg', image)
-            encoded_image = base64.b64encode(buffer).decode('utf-8')
-            image_data = f'data:image/jpeg;base64,{encoded_image}'
+                    return render(request, 'result.html', {
+                        'image_data': image_data,
+                        'predicted_label': "Unknown Image, please try again!",
+                        'accuracy': 'N/A'
+                    })
 
-            # Prepare the response
-            context = {
-                'image_data': image_data,
-                'predicted_label': predicted_label,
-                'accuracy': f'{accuracy:.2f}%'
-            }
-            return render(request, 'result.html', context)
+                # Convert the image to Base64 for displaying on the web
+                _, buffer = cv2.imencode('.jpg', image)
+                encoded_image = base64.b64encode(buffer).decode('utf-8')
+                image_data = f'data:image/jpeg;base64,{encoded_image}'
+
+                # Prepare the response
+                context = {
+                    'image_data': image_data,
+                    'predicted_label': predicted_label,
+                    'accuracy': f'{accuracy:.2f}%'
+                }
+                return render(request, 'result.html', context)
+            except Exception as e:
+                form.add_error(None, f"Error processing image: {str(e)}")
     else:
         form = ImageUploadForm()
 
